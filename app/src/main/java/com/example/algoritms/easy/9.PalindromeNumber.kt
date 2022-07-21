6a07ab2c59a465a61e498ba8f69f898337708c0e
xeyal21 <serifli2003@mail.ru>
2022-07-21 15:16:53 +0400

    9

--- app/src/main/java/com/example/algoritms/easy/9.PalindromeNumber.kt
+++ app/src/main/java/com/example/algoritms/easy/9.PalindromeNumber.kt
@@ -5,11 +5,10 @@ An integer is a palindrome when it reads the same backward as forward.
 For example, 121 is a palindrome while 123 is not.
 */
 fun main() {
-    println(isPalindrome(124))
-    println(isPalindrome(121))
+    println(isPalindrome(-121))
 }
 
 fun isPalindrome(x: Int): Boolean {
-    val reverseInt = x.toString().reversed().toInt()
-    return x == reverseInt
+    val reverseInt = x.toString().reversed()
+    return x.toString() == reverseInt
 }
